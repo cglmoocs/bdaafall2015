9e7677fd20484afed54c0abe77fedcac63d243b8
Hyungro Lee <hroe.lee@gmail.com>
2015-09-28 18:56:20 -0400

    minor

--- PythonFiles/FirstProgramWithSystemInfo.py
+++ PythonFiles/FirstProgramWithSystemInfo.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import getpass
 import socket
 
-# Run python FirstProgram.py
+# Run python FirstProgramWithSystemInfo.py
 print ('My first program with System Information!')
 
 print ("Today is: " + str(datetime.now()))
